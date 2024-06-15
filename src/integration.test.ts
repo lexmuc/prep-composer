be2bd0db92bc68327ba4708f7f24991350f02752
Alexander Steinhoff <git@alexander-steinhoff.net>
2024-06-15 13:49:31 +0200

    added integration test using sql.js and sqlstring

---
+++ src/integration.test.ts
@@ -0,0 +1,82 @@
+// noinspection SqlNoDataSourceInspection,SqlDialectInspection
+
+import initSqlJs, {Database} from 'sql.js';
+import SqlString from 'sqlstring';
+import {$, sql, SqlSegment} from './index';
+
+const escapeFn = (value: any) => {
+    if (typeof value === 'string') {
+        return "'" + value.replace(/'/g, "''") + "'";
+    }
+    else return SqlString.escape(value);
+};
+
+const $create = sql(
+    'CREATE TABLE employees (',
+    'employee_id INT PRIMARY KEY,',
+    'name VARCHAR(50),',
+    'hire_date DATE',
+    ')'
+);
+
+const name1 = "O'Brien";
+const name2 = 'Doe "John"';
+const $insert = sql(
+    'INSERT INTO employees (employee_id, name, hire_date) VALUES (',
+    $(1), ', ', $(name1), ', ', $('2020-01-01'),
+    '), (',
+    $(2), ', ', $(name2), ', ', $('2020-01-02'),
+    ')'
+);
+
+const $select = sql('SELECT * FROM employees ORDER BY employee_id');
+
+const runWithEscaping = (db: Database, seg: SqlSegment) => db.exec(seg.toString(escapeFn));
+
+const queryWithEscaping = (db: Database, seg: SqlSegment) => db.exec(seg.toString(escapeFn))[0].values;
+
+const runPrepared = (db: Database, seg: SqlSegment) => {
+    const stmt = db.prepare(seg.toString());
+    // @ts-ignore
+    stmt.run(seg.parameters);
+    stmt.free();
+}
+
+const queryPrepared = (db: Database, seg: SqlSegment) => {
+    const stmt = db.prepare(seg.toString());
+    // @ts-ignore
+    stmt.bind(seg.parameters);
+    const rows = [];
+    while (stmt.step()) {
+        rows.push(stmt.get());
+    }
+    stmt.free();
+    return rows;
+}
+
+test('it can be used with an escape function to create a table, add rows and read from it', async () => {
+    const SQL = await initSqlJs();
+    const db = new SQL.Database();
+
+    runWithEscaping(db, $create);
+    runWithEscaping(db, $insert);
+
+    expect(queryWithEscaping(db, $select)).toEqual([
+        [1, name1, '2020-01-01'],
+        [2, name2, '2020-01-02'],
+    ]);
+});
+
+test('it can be used with prepared statements to create a table, add rows and read from it', async () => {
+    const SQL = await initSqlJs();
+    const db = new SQL.Database();
+    runPrepared(db, $create);
+    runPrepared(db, $insert);
+
+    const $select = sql('SELECT * FROM employees ORDER BY employee_id');
+    const rows = queryPrepared(db, $select);
+    expect(rows).toEqual([
+        [1, name1, '2020-01-01'],
+        [2, name2, '2020-01-02'],
+    ]);
+});
